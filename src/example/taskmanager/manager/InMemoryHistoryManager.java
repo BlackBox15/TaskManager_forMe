4a5851f3134fca6bbcd7d69b3f903c68f48d7f8d
mk1221 <mk1221@yandex.ru>
2022-09-05 16:05:49 +0300

    new: taskList

--- src/example/taskmanager/manager/InMemoryHistoryManager.java
+++ src/example/taskmanager/manager/InMemoryHistoryManager.java
@@ -1,13 +1,43 @@
 package example.taskmanager.manager;
 
 import example.taskmanager.task.Task;
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager{
 
     private final Integer MAX_HISTORY = 10;
     private final List<Task> historyList = new ArrayList<>();
+    private Map<Task, Node> historyMap = new HashMap<>();
+    private List<Task> taskList = new LinkedList<>();
+    private Node<Task> head;
+    // Указатель на последний элемент списка. Он же last
+    private Node<Task> tail;
+    private int size = 0;
+
+
+    public void linkLast(Task element) {
+        Node<Task> oldTail = tail;
+        Node<Task> newNode = new Node<>(oldTail, element, null);
+        tail = newNode;
+        oldTail.next = newNode;
+        size++;
+    }
+
+    public void removeNode(Node node)  {
+
+    }
+
+    public ArrayList<Task> getTasks() {
+        ArrayList<Task> arrayList = new ArrayList<>();
+
+        for (Task task :
+                taskList) {
+            arrayList.add(task);
+        }
+
+        return arrayList;
+    }
 
     @Override
     public void remove(int id) {
